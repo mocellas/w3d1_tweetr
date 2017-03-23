2111adec97feb43d32812fe4e75718435f3eb63a
mocellas <fernando.mocellin@gmail.com>
2017-03-23 17:25:19 +0000

    Implement renderTweets function

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,41 +5,25 @@
  */
 
 function createTweetElement(tweetObj) {
-    var $tweet = $('<article></article>');
-    // Header
-    var $header = $('<header></header>');
-    var $avatar = $('<img>').addClass('tweet-thumbnail');
-    $avatar.attr('src',tweetObj.user.avatars.small);
-    $header.append($avatar);
-    //var $headerText = $('<h3></h3>').text(tweetObj.user.name);
-    //$header.append($headerText);
-    var $username = $(`<div class="hardcoded">${tweetObj.user.handle}</div>`);
-        $header.append($username);
-        $tweet.append($header);
+    var tweet = `<article>
+          <header>
+            <img src="/images/Ernest.png"  width="60px" height="60px" border-style="solid"> ${tweetObj.user.name}
+          </header>
+          <div>
+          <p>This tweet was posted a few minutes ago</p> ${tweetObj.user.content}
+          </div>
+            <footer> ${tweetObj.user.createdAt}
+              <p> 10 days ago</p>
+              <i class="fa fa-retweet" aria-hidden="true"></i>
+              <i class="fa fa-heart" aria-hidden="true"></i>
+              <i class="fa fa-flag" aria-hidden="true"></i>
+            </footer>
+        </article>`;
 
-    // Body
-    var $tweetText = $('<p></p>').text(tweetObj.content.text);
-    $tweet.append($tweetText);
-
-    // Footer
-    var $footer = $('<footer></footer>');
-    var postTimeDiff = new Date(new Date().getTime() - new Date(tweetObj.created_at).getTime());
-    var postDaysAgo = Math.round(postTimeDiff/1000/60/60/24);
-    var $span = $('<span></span>').addClass('tweet-timestamp').text(postDaysAgo + " days ago");
-    $footer.append($span);
-    var $div = $('<div></div>').addClass('tweet-interact-icons');
-
-    //Hover
-    var $iconsRetweet = $('<i class="fa fa-retweet" aria-hidden="true"></i>');
-    var $iconsHeart = $('<i class="fa fa-heart" aria-hidden="true"></i>');
-    var $iconsFlag = $('<i class="fa fa-flag" aria-hidden="true"></i>');
-    $div.append($iconsFlag).append($iconsRetweet).append($iconsHeart);
-    $footer.append($div);
-    $tweet.append($footer);
-    return $tweet;
+    // <div id="one">one</div>
+    var d1 = document.getElementById('new-tweets');
+    d1.insertAdjacentHTML('afterbegin', tweet);
 }
-
-
 
 function renderTweets(tweetsArr) {
     tweetsArr.forEach((tweet) => {
